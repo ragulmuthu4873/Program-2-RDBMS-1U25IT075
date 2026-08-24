fba42197eb3ee72c44b2b7c7141336c6278400ce
RAGUL M <ragulmuthu38@gmail.com>
2026-08-24 11:02:13 +0530

    Program-2 RDBMS 1U25IT075

--- solution.sql
+++ solution.sql
@@ -1,1 +1,13 @@
 
+create database CollegeDB;
+use CollegeDB;
+
+create table Student (
+    StudentID INT(5) NOT NULL PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
+
+describe Student;
